Fix guided backprop, add guided GradCAM
relu activations (including layers.ReLU) are now properly replaced, all multi-layer plot functions now use tqdm progress bars, slew of other minor tweaks and changes

layervis/features.py
@@ -21,11 +21,10 @@ from keras import layers
 import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.figure import Figure
+from tqdm import tqdm
 import os
 
-from layervis.utils import (
-    get_layer_object, obtain_reasonable_figsize, save_image
-)
+import layervis.utils as lvutils
 
 class FeatureMaps:
     """
@@ -37,7 +36,7 @@ class FeatureMaps:
 
     def __init__(
             self, model: keras.Model,
-            valid_layers: tuple[layers.Layer] = None):
+            valid_layers: tuple[layers.Layer, ...] = None):
         """
         Initialises a FeatureMaps object with the provided model and layers to extract.
 
@@ -77,13 +76,14 @@ class FeatureMaps:
         Raises:
             ValueError if the provided layer is not a valid layer.
         """
-        layer = get_layer_object(self.model, layer)
+        layer = lvutils.get_layer_object(self.model, layer)
         if not isinstance(layer, self.valid_layers):
             raise ValueError(f'Invalid layer, must be one of {self.valid_layers}')
+        input_image = tf.convert_to_tensor(input_image)
         
         keras.backend.clear_session()
         feature_model = keras.Model(inputs=self.model.input, outputs=layer.output)
-        feature_maps = feature_model.predict(input_image, verbose=0)
+        feature_maps = feature_model(input_image).numpy()
         if len(feature_maps.shape) != 4:
             print(
                 f'Skipping layer {layer.name} '
@@ -133,7 +133,7 @@ class FeatureMaps:
             return None
         # determine figure dimensions
         nmaps = feature_maps.shape[-1]
-        nrows, ncols = obtain_reasonable_figsize(
+        nrows, ncols = lvutils.obtain_reasonable_figsize(
             nmaps, aspect_mode=fig_aspect, orient=fig_orient
         )
         fig = plt.figure(figsize=(figscale*ncols, figscale*nrows), dpi=dpi)
@@ -154,13 +154,13 @@ class FeatureMaps:
 
     def plot_feature_maps(
             self, input_image: tf.Tensor | np.ndarray,
-            layers: list[int | str | layers.Layer] = [],
+            layers_list: list[int | str | layers.Layer] = [],
             plot_input: bool = True, figscale: float = 1, dpi: float = 100,
             colormap: str = 'cividis', facecolor: str = 'white',
             fig_aspect: str = 'uniform', fig_orient: str = 'h',
             save_dir: str = 'feature_maps', save_format: str = 'png', prefix: str = '',
             suffix: str = '', include_titles: bool = False,
-            include_corner_axis: bool = False, verbose: bool = True) -> None:
+            include_corner_axis: bool = False) -> None:
         """
         Plots and saves all feature maps for all convolutional and pooling
         layers with respect to the provided (single) test image. Ensure that
@@ -171,7 +171,7 @@ class FeatureMaps:
         Args:
             input_image: Single input with which to generate feature maps.
                 Its shape must be the same as the model's input shape.
-            layers: List of layer names and/or indices of the layers to extract
+            layers_list: List of layer names and/or indices of the layers to extract
                 features from. If empty, the code will process all valid layers.
             plot_input: Whether to also plot the input image. Defaults to True.
             figscale: Base figure size multiplier, passed to plt.figure. Default is 1.
@@ -194,14 +194,12 @@ class FeatureMaps:
                 of the respective layer. Defaults to True.
             include_corner_axis: Whether to display axes on the
                 bottom-left subplot of each figure. Defaults to False.
-            verbose: Whether to print each filename as it is saved. Defaults to True.
         """
-        if len(layers) == 0:
-            layers = [
-                i.name for i in self.model.layers if isinstance(i, self.valid_layers)
-            ]
-        else:
-            layers = [get_layer_object(self.model, l).name for l in layers]
+        layers_list = (
+            [i for i in self.model.layers if isinstance(i, self.valid_layers)]
+            if len(layers_list) == 0
+            else [lvutils.get_layer_object(self.model, l) for l in layers]
+        )
 
         try:
             os.mkdir(save_dir)
@@ -209,14 +207,14 @@ class FeatureMaps:
             pass
         
         if plot_input:
-            save_image(
+            lvutils.save_image(
                 image=input_image[0, ...], figsize=figscale*6, dpi=dpi,
                 colormap=colormap, facecolor=facecolor, save_dir=save_dir,
                 filename=f'input{suffix}', save_format=save_format,
                 figure_title=('input' if include_titles else None),
-                include_axis=include_corner_axis, verbose=verbose
+                include_axis=include_corner_axis
             )
-        for layer in layers:
+        for layer in tqdm(layers_list):
             fig = self.plot_feature_map(
                 input_image=input_image, layer=layer, figscale=figscale, dpi=dpi,
                 colormap=colormap, fig_aspect=fig_aspect, fig_orient=fig_orient,
@@ -226,12 +224,10 @@ class FeatureMaps:
                 continue
             file_name = os.path.join(
                 f'{save_dir}',
-                f'{prefix}{layer}{suffix}.{save_format}'
+                f'{prefix}{layer.name}{suffix}.{save_format}'
             )
             fig.savefig(
                 file_name, format=save_format, facecolor=facecolor, bbox_inches='tight'
             )
-            if verbose:
-                print(f'Saved {file_name}')
             fig.clear()
             plt.close(fig)

layervis/filters.py
@@ -23,11 +23,10 @@ import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.figure import Figure
 from skimage.filters import gaussian
+from tqdm import tqdm
 import os
 
-from layervis.utils import (
-    get_blank_image, get_layer_object, obtain_reasonable_figsize
-)
+import layervis.utils as lvutils
 
 class FilterWeights:
     """
@@ -41,11 +40,13 @@ class FilterWeights:
     
     def __init__(self, model: keras.Model):
         self.model = model
-        self.valid_layers = (layers.Conv2D, layers.Conv2DTranspose)
+        self.valid_layers: tuple[layers.Layer, ...] = (
+            layers.Conv2D, layers.Conv2DTranspose,
+        )
         
 
     def plot_filter_weights(
-            self, layer: int | str | tf.Tensor, input_map_index: int = 0,
+            self, layer: int | str | tf.Tensor, input_channel_index: int = 0,
             normalize_weights: bool = True, figscale: float = 1, dpi: float = 100,
             colormap: str = 'cividis', fig_aspect: str = 'uniform',
             fig_orient: str = 'h', include_title: bool = False) -> Figure:
@@ -55,9 +56,10 @@ class FilterWeights:
         Args:
             layer: Name of the layer to extract filters from.
                 The layer itself must be a Conv2D layer.
-            input_map_index:
-                The input feature map to extract filters from
-                e.g. First layer typically has 1 (mono) or 3 (RGB) inputs
+            input_channel_index: The index of the input channel to extract the filters
+                from. Each conv layer has N input channels where N is the number of
+                output filters in the previous conv layer. The first conv layer
+                typically has 1 (mono) or 3 (RGB) input channels.
             normalize_weights: Whether to normalize the filter weights before
                 visualisation. Default is True.
             figscale: Base figure scale multiplier, passed to plt.figure.
@@ -76,16 +78,15 @@ class FilterWeights:
         Raises:
             ValueError if attempting to call with a non-convolutional layer
         """
-        layer = get_layer_object(model=self.model, layer_spec=layer)
+        layer = lvutils.get_layer_object(model=self.model, layer_spec=layer)
         if not isinstance(layer, self.valid_layers):
             raise ValueError(f'Invalid layer, must be one of {self.valid_layers}')
 
         filters = layer.get_weights()[0]
-        # extract the filters for the given input_map_index
-        # only if the filter input isn't an RGB image
-        is_rgb = filters.shape[-2] == 3
-        if not is_rgb:
-            filters = filters[..., input_map_index, :]
+        # extract just the filters for the specified input channel index
+        # unless the input is 3 channels (in which case we plot an RGB image)
+        if filters.shape[-2] != 3:
+            filters = filters[..., input_channel_index, :]
         if normalize_weights:
             filters = (
                 (filters - np.min(filters))
@@ -94,7 +95,7 @@ class FilterWeights:
         filters = np.uint8(filters*255)
 
         num_filters = filters.shape[-1]
-        nrows, ncols = obtain_reasonable_figsize(
+        nrows, ncols = lvutils.obtain_reasonable_figsize(
             num_filters, aspect_mode=fig_aspect, orient=fig_orient
         )
         fig = plt.figure(figsize=(figscale*ncols, figscale*nrows), dpi=dpi)
@@ -109,38 +110,38 @@ class FilterWeights:
 
 
     def plot_all_weights(
-            self, input_map_index: int = 0, normalize_weights: bool = True,
-            save_dir: str = 'filter_weights', save_format: str = 'png',
+            self, input_channel_index: int = 0, normalize_weights: bool = True,
             figscale: float = 1, dpi: float = 100, colormap: str = 'cividis',
-            fig_aspect: str = 'uniform', fig_orient: str = 'h',
-            facecolor: str = 'white', include_titles: bool = False, prefix: str = '',
-            suffix: str = '', verbose: bool = True) -> None:
+            facecolor: str = 'white', include_titles: bool = False,
+            save_dir: str = 'filter_weights', save_format: str = 'png',
+            save_str: str = '', fig_aspect: str = 'uniform',
+            fig_orient: str = 'h') -> None:
         """
         Plots the filter weights for all valid layers.
 
         Args:
-            input_map_index: The input feature map to extract filters from
-                e.g. First layer typically has 1 (mono) or 3 (RGB) inputs
+            input_channel_index: The index of the input channel to extract the filters
+                from. Each conv layer has N input channels where N is the number of
+                output filters in the previous conv layer. The first conv layer
+                typically has 1 (mono) or 3 (RGB) input channels.
             normalize_weights: Whether to normalize the filter weights before
                 visualisation. Default is True.
-            save_dir: Directory in which to save the images
-            save_format: Format with which to save the images
             figscale: Base figure size, passed to plt.figure
                 Note the dimensions of the figure are automatically determined,
                 figscale is merely a multiplier with which to scale.
             dpi: Base resolution, passed to plt.figure
-            colormap: Base colormap, passed to plt.figure
+            colormap: Base colormap, passed to plt.figure.
+            facecolor: Background color, passed to fig.savefig.
+            include_titles: Whether to title each figure with the name
+                of each layer. Defaults to False.
+            save_dir: Directory in which to save the images
+            save_format: Format with which to save the images
+            save_str: Optional string to name the output file. The output format is
+                fweights_{save_str}{layer.name}.{save_format}. Default is ''.
             fig_aspect: One of 'uniform' or 'wide'. Use 'uniform' for
                 squarish plots and 'wide' for rectangular.
             fig_orient: One of 'h' or 'v'. If set to 'h', the number of columns
                 will be >= the number of rows (vice versa if set to 'v').
-            facecolor: Background color, passed to fig.savefig.
-            include_titles: Whether to title each figure with the name
-                of each layer. Defaults to False.
-            prefix: Optional prefix to add to the filename of each image.
-            suffix: Optional suffix to add to the filename of each image.
-            verbose: Whether to print the name of each file as it is
-                saved. Defaults to True.
         """
         try:
             os.mkdir(save_dir)
@@ -150,18 +151,16 @@ class FilterWeights:
         conv_layers = [
             i.name for i in self.model.layers if isinstance(i,self.valid_layers)
         ]
-        for layer in conv_layers:
+        for layer in tqdm(conv_layers):
             fig = self.plot_filter_weights(
-                layer, input_map_index=input_map_index,
+                layer, input_channel_index=input_channel_index,
                 normalize_weights=normalize_weights, figscale=figscale, dpi=dpi,
                 colormap=colormap, fig_aspect=fig_aspect, fig_orient=fig_orient,
                 include_title=include_titles
             )
             filename = os.path.join(
-                save_dir, f'{prefix}{layer}{suffix}.{save_format}'
+                save_dir, f'fweights_{save_str}{layer}.{save_format}'
             )
-            if verbose:
-                print(f'Saving to {filename}')
             fig.savefig(
                 filename, format=save_format, facecolor=facecolor, bbox_inches='tight'
             )
@@ -182,7 +181,9 @@ class FilterVis:
     
     def __init__(
             self, model: keras.Model,
-            valid_layers: tuple[layers.Layer] = (layers.Conv2D, layers.Conv2DTranspose),
+            valid_layers: tuple[layers.Layer, ...] = (
+                layers.Conv2D, layers.Conv2DTranspose,
+            ),
             scale_factor: float = 0.2, brightness_factor: float = 0.4,
             image_decay: float = 0.8, enable_blur: bool = True,
             blur_freq: int = 5, blur_size: int = 3):
@@ -312,7 +313,7 @@ class FilterVis:
             A np.array representing the image of the given filter
         """
         image = (
-            get_blank_image(
+            lvutils.get_blank_image(
                 shape=(1, *extractor.input_shape[1:]),
                 scale_factor=self.scale_factor,
                 brightness_factor=self.brightness_factor
@@ -351,6 +352,56 @@ class FilterVis:
         image = (image - np.min(image)) / (np.ptp(image) + keras.backend.epsilon())
         image *= 255
         return np.clip(image, 0, 255).astype('uint8')
+    
+
+    def get_filter_patterns(
+            self, layer: str | int | tf.Tensor,
+            initial_image: np.ndarray | tf.Tensor = None,
+            num_iterations: int = 30, learning_rate: float = 10,
+            activation_crop: int = 1, image_crop: int = 0) -> np.ndarray:
+        """
+        Get filter patterns for all filters in the specified layer.
+
+        Args:
+            layer: Name or index of the keras.layers.Conv2D layer to visualise
+            initial_image: An optional image to run the gradient ascent on.
+                If set to None, a visually neutral "blank" image is used instead.
+                Defaults to None. Image shape must be (1, size, size, depth).
+            num_iterations: The number of gradient ascent steps
+            learning_rate: Learning rate for each gradient ascent step
+            activation_crop: Amount by which to crop the filter activation.
+                This is used to avoid border artifacts, and should therefore be kept
+                small. Defaults to 1.
+            image_crop: Amount by which to crop the final image during
+                postprocessing. Defaults to 0.
+        
+        Returns:
+            Filter patterns as a numpy array.
+
+        Raises:
+            ValueError if trying to call on an invalid layer
+        """
+        layer = lvutils.get_layer_object(model=self.model, layer_spec=layer)
+        if not isinstance(layer, self.valid_layers):
+            raise ValueError(f'Invalid layer, must be one of {self.valid_layers}')
+
+        activation_crop = max(0, activation_crop)
+        image_crop = max(0, image_crop)
+        if initial_image is not None:
+            initial_image = tf.convert_to_tensor(initial_image)
+
+        keras.backend.clear_session()
+        extractor = keras.Model(inputs=self.model.input, outputs=layer.output)
+        num_filters = extractor.output_shape[-1]
+        filter_patterns = np.array()
+        for i in range(num_filters):
+            image = self.process_filter(
+                i, initial_image, extractor,
+                num_iterations, learning_rate, activation_crop
+            )
+            image = self.postprocess_image(image, image_crop)
+            np.append(filter_patterns, image)
+        return filter_patterns
 
 
     def plot_filter_pattern(
@@ -395,7 +446,7 @@ class FilterVis:
         Raises:
             ValueError if trying to call on an invalid layer
         """
-        layer = get_layer_object(model=self.model, layer_spec=layer)
+        layer = lvutils.get_layer_object(model=self.model, layer_spec=layer)
         if not isinstance(layer, self.valid_layers):
             raise ValueError(f'Invalid layer, must be one of {self.valid_layers}')
 
@@ -408,7 +459,7 @@ class FilterVis:
         extractor = keras.Model(inputs=self.model.input, outputs=layer.output)
 
         num_filters = extractor.output_shape[-1]
-        nrows, ncols = obtain_reasonable_figsize(
+        nrows, ncols = lvutils.obtain_reasonable_figsize(
             num_filters, aspect_mode=fig_aspect, orient=fig_orient
         )
         fig = plt.figure(figsize=(figscale*ncols, figscale*nrows), dpi=dpi)
@@ -438,8 +489,8 @@ class FilterVis:
             figscale: float = 1, dpi: float = 100, colormap: str = 'binary_r',
             fig_aspect: str = 'uniform', fig_orient: str = 'h', facecolor: str = 'white',
             save_dir: str = 'filter_patterns', save_format: str = 'png',
-            prefix: str = '', suffix: str = '', include_titles: bool = False,
-            include_corner_axis: bool = False, verbose: bool = True) -> None:
+            save_str: str = '', include_titles: bool = False,
+            include_corner_axis: bool = False) -> None:
         """
         Visualises and plots the filter patterns of all valid layers.
         Each plot is saved to save_dir.
@@ -468,8 +519,8 @@ class FilterVis:
                 Each image is named according to the layername.
             save_format: Format with which to save each image
                 e.g 'png', 'pdf', 'jpg'
-            prefix: Prefix to prepend to each filename.
-            suffix: Suffix to append to each filename.
+            save_str: Optional string to name the output file. The output format is
+                fvis_{save_str}{layer.name}.{save_format}. Default is ''.
             include_titles: Whether to include titles in each figure.
                 Defaults to False.
             include_corner_axis: Whether to include axes
@@ -481,13 +532,12 @@ class FilterVis:
         except FileExistsError:
             pass
 
-        layer_names = [
-            layer.name for layer in self.model.layers
-            if isinstance(layer, self.valid_layers)
+        layers_list = [
+            l for l in self.model.layers if isinstance(l, self.valid_layers)
         ]
-        for layer_name in layer_names:
+        for layer in tqdm(layers_list):
             fig = self.plot_filter_pattern(
-                layer_name, initial_image=initial_image,
+                layer=layer, initial_image=initial_image,
                 num_iterations=num_iterations, learning_rate=learning_rate,
                 activation_crop=activation_crop, image_crop=image_crop,
                 figscale=figscale, dpi=dpi, colormap=colormap, fig_aspect=fig_aspect,
@@ -495,10 +545,8 @@ class FilterVis:
                 include_corner_axis=include_corner_axis
             )
             filename = os.path.join(
-                save_dir, f'{prefix}{layer_name}{suffix}.{save_format}'
+                save_dir, f'fvis_{save_str}{layer.name}.{save_format}'
             )
-            if verbose:
-                print(f'Saving to {filename}')
             fig.savefig(
                 filename, format=save_format, facecolor=facecolor, bbox_inches='tight'
             )

layervis/utils.py
@@ -18,12 +18,37 @@
 import tensorflow as tf
 from tensorflow import keras
 from keras import layers
+from keras.preprocessing.image import array_to_img, img_to_array
 import numpy as np
 import matplotlib.pyplot as plt
 import os
 
+@tf.custom_gradient
+def guided_relu(x):
+    """
+    Custom gradient to apply for guided backpropagation.
+    """
+    def grad(dy):
+        return tf.cast(dy > 0, 'float32') * tf.cast(x > 0, 'float32') * dy
+    return tf.nn.relu(x), grad
+
+class GuidedReLU(layers.Layer):
+    """
+    Custom layers.Layer class for guided_relu activation.
+    It is intended to replace existing ReLU layers for guided backpropagation.
+    """
+
+    def __init__(self, **kwargs):
+        """Instantiate a custom GuidedReLU layer for guided backpropagation."""
+        super(GuidedReLU, self).__init__(**kwargs)
+    
+    def call(self, inputs):
+        """Use guided relu instead of the normal relu"""
+        return guided_relu(inputs)
+    
+
 def get_blank_image(
-        shape: tuple[int], scale_factor: float = 0.2,
+        shape: tuple[int, ...], scale_factor: float = 0.2,
         brightness_factor: float = 0.4) -> tf.Tensor:
     """
     Creates a blank, visually neutral image (predominantly gray).
@@ -80,10 +105,19 @@ def get_layer_object(
         raise TypeError(f'{layer_spec} is not a valid layer specifier.')
 
 
+def get_last_conv2d_layer(model: keras.Model) -> layers.Layer:
+    """
+    Returns the last 2D convolutional layer in the given model.
+    """
+    for l in model.layers[::-1]:
+        if isinstance(l, layers.Conv2D):
+            return l
+
+
 def save_image(
         image: tf.Tensor | np.ndarray, figsize: float, dpi: float, colormap: str,
         facecolor: str, save_dir: str, filename: str, save_format: str,
-        figure_title: str, include_axis: bool, verbose: bool) -> None:
+        figure_title: str, include_axis: bool) -> None:
     """
     Plots and saves the given image according to the provided
     figure and filename parameters.
@@ -102,7 +136,6 @@ def save_image(
         figure_title: Title to add to the figure.
             Note that no title is added if this is set to None or ''
         include_axis: Whether to include an axis.
-        verbose: Whether to include print statements.
     """
     if isinstance(image, tf.Tensor):
         image = image.numpy()
@@ -115,8 +148,6 @@ def save_image(
     if not include_axis:
         plt.axis('off')
     file_name = os.path.join(save_dir, f'{filename}.{save_format}')
-    if verbose:
-        print(f'Saving to {file_name}')
     fig.savefig(
         file_name, format=save_format,
         facecolor=facecolor, bbox_inches='tight'
@@ -125,7 +156,7 @@ def save_image(
     plt.close(fig)
 
 
-def euclidean_dist_2d(a: tuple[float], b: tuple[float]) -> float:
+def euclidean_dist_2d(a: tuple[float, float], b: tuple[float, float]) -> float:
     """
     Returns the Euclidean distance between a and b where a, b are 2D coordinates.
     Curiously, this is around 10% faster than np.linalg.norm()
@@ -135,7 +166,7 @@ def euclidean_dist_2d(a: tuple[float], b: tuple[float]) -> float:
 
 def obtain_reasonable_figsize(
         num_subplots: int, aspect_mode: str = 'uniform',
-        orient: str = 'h') -> tuple[int]:
+        orient: str = 'h') -> tuple[int, int]:
     """
     Automatically determines somewhat aesthetically pleasing figure dimensions
     given the required number of subplots to plot. Dimensions will be a tuple